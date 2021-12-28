4761f6aa34dcd9254748f3d02c0e22cfc6b6168f
Joakim Hellsén <tlovinator@gmail.com>
2021-12-28 22:32:12 +0100

    Return HTTP_200_OK instead of HTTP_201_CREATED

--- twitter_image_collage_maker/main.py
+++ twitter_image_collage_maker/main.py
@@ -68,12 +68,12 @@ async def add(tweet_id: int) -> JSONResponse:
         if os.path.isfile(f"{Settings.static_location}/tweets/{tweet_id}.png"):
             json_content = {"url": f"{Settings.url}/static/tweets/{tweet_id}.png"}
             hook.send(f"Already had a image for tweet https://twitter.com/i/status/{tweet_id}\n`{json_content}`")
-            return JSONResponse(status_code=status.HTTP_201_CREATED, content=json_content)
+            return JSONResponse(status_code=status.HTTP_200_OK, content=json_content)
 
         # Otherwise download the image and return the URL to the image
         json_content = download_images(tweet_id, api, hook)
         hook.send(f"Returned image for tweet https://twitter.com/i/status/{tweet_id}\n`{json_content}`")
-        return JSONResponse(status_code=status.HTTP_201_CREATED, content=json_content)
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json_content)
 
     except Exception as e:
         print("Error: " + str(e))
